ef915686f1dd8d8a8fd0465972c661e8de13d618
ross <92001561+z0r0z@users.noreply.github.com>
2023-09-22 18:25:06 +0000

    ~~

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -70,34 +70,35 @@ contract Wallet {
     function _isValidSignature(bytes32 hash, bytes calldata signature)
         internal
         view
-        returns (uint flag)
+        returns (uint isValid)
     {
         bytes32 _owner = owner;
         assembly ("memory-safe") {
-            let m := mload(0x40) // Cache the free memory pointer.
-            mstore(0x00, hash)
-            mstore(0x20, byte(0, calldataload(add(signature.offset, 0x40)))) // `v`.
-            calldatacopy(0x40, signature.offset, 0x40) // Copy `r` and `s`.
-            let result :=
-                mload(
+            let m := mload(0x40)
+            isValid := 1
+
+            if eq(signature.length, 65) {
+                mstore(0x00, hash)
+                mstore(0x20, byte(0, calldataload(add(signature.offset, 0x40)))) // `v`.
+                calldatacopy(0x40, signature.offset, 0x40) // `r`, `s`.
+                let t :=
                     staticcall(
                         gas(), // Amount of gas left for the transaction.
-                        eq(signature.length, 65), // Address of `ecrecover`.
+                        1, // Address of `ecrecover`.
                         0x00, // Start of input.
                         0x80, // Size of input.
                         0x01, // Start of output.
                         0x20 // Size of output.
                     )
-                )
-            // `returndatasize()` will be `0x20` upon success, and `0x00` otherwise.
-            if iszero(returndatasize()) { flag := 1 }
-            if iszero(eq(_owner, result)) { flag := 1 }
+                // `returndatasize()` will be `0x20` upon success, and `0x00` otherwise.
+                if iszero(or(iszero(returndatasize()), xor(_owner, mload(t)))) { isValid := 0 }
+            }
             mstore(0x60, 0) // Restore the zero slot.
             mstore(0x40, m) // Restore the free memory pointer.
 
-            if eq(flag, 1) {
+            if eq(isValid, 1) {
                 let f := shl(224, 0x1626ba7e)
-                mstore(m, f) // `bytes4(keccak256('isValidSignature(bytes32,bytes)'))`.
+                mstore(m, f) // `bytes4(keccak256("isValidSignature(bytes32,bytes)"))`.
                 mstore(add(m, 0x04), hash)
                 let d := add(m, 0x24)
                 mstore(d, 0x40) // The offset of the `signature` in the calldata.
@@ -105,7 +106,7 @@ contract Wallet {
                 // Copy the `signature` over.
                 calldatacopy(add(m, 0x64), signature.offset, signature.length)
                 // forgefmt: disable-next-item
-                flag := iszero(and(
+                isValid := iszero(and(
                     // Whether the returndata is the magic value `0x1626ba7e` (left-aligned).
                     eq(mload(d), f),
                     // Whether the staticcall does not revert.
