f9b62d45479c588266f9e3fe6b70b80c678f5ffe
Shivanshi Tyagi <shivanshityagi@icloud.com>
2023-09-23 01:33:21 +0530

    ✓

--- test/Wallet.t.sol
+++ test/Wallet.t.sol
@@ -10,6 +10,7 @@ import '@forge/Test.sol';
 
 import {MockERC721} from '@solady/test/utils/mocks/MockERC721.sol';
 import {MockERC1155} from '@solady/test/utils/mocks/MockERC1155.sol';
+import {MockERC1271Wallet} from "@solady/test/utils/mocks/MockERC1271Wallet.sol";
 
 contract WalletTest is Test {
     address aliceAddr;
@@ -23,12 +24,17 @@ contract WalletTest is Test {
     address constant entryPoint = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
 
     Wallet w;
+    Wallet contractOwnedW;
     MockERC721 erc721;
     MockERC1155 erc1155;
+    MockERC1271Wallet contractWallet;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     function setUp() public payable {
+        // Create Ethereum fork.
+        vm.createSelectFork(vm.rpcUrl("main"));
+
         (aliceAddr, aliceKey) = makeAddrAndKey('alice');
         (bobAddr, bobKey) = makeAddrAndKey('bob');
 
@@ -41,6 +47,9 @@ contract WalletTest is Test {
 
         erc721 = new MockERC721();
         erc1155 = new MockERC1155();
+
+        contractWallet = new MockERC1271Wallet(address(aliceAddr));
+        contractOwnedW = f.deploy(bytes32(uint(uint160(address(contractWallet)))));
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -98,6 +107,17 @@ contract WalletTest is Test {
         assert(selector == 0x1626ba7e);
     }
 
+    function testIsValidSignatureFromContract() public payable {
+        bytes32 hash = keccak256(bytes('FOO'));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(aliceKey, hash);
+        bytes memory sig = abi.encodePacked(r, s, v);
+
+        bytes4 selector = contractWallet.isValidSignature(hash, sig);
+
+        assert(selector == 0x1626ba7e);
+    }
+
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     function testOnERC721Received() public payable {
@@ -120,5 +140,13 @@ contract WalletTest is Test {
         uint[] memory amts = new uint256[](1);
         amts[0] = 1;
         erc1155.safeBatchTransferFrom(aliceAddr, address(w), ids, amts, '');
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    function testValidateUserOp() public payable {
+        vm.prank(entryPoint);
+
+        wallet.validateUserOp(userOp)
     }
 }
