4f802ed7cb133e38b97f2986f43a8096c6d29014
ross <92001561+z0r0z@users.noreply.github.com>
2023-09-22 14:20:15 +0000

    ~~

--- src/WalletFactory.sol
+++ src/WalletFactory.sol
@@ -4,7 +4,21 @@ pragma solidity ^0.8.19;
 import {Wallet} from "./Wallet.sol";
 
 contract WalletFactory {
-    function deploy(bytes32 owner) public payable returns (Wallet wallet) {
-        return new Wallet{value: msg.value, salt: owner}(owner);
+    function deploy(bytes32 ownerAndSalt) public payable returns (Wallet wallet) {
+        // Prepare just the contract creation code outside of assembly
+        bytes memory code = type(Wallet).creationCode;
+        // Perform the append and create2 operation within assembly
+        assembly {
+            let codeSize := mload(code) // Get the size of the creation code
+            let codeData := add(code, 0x20) // Skip the length field to get to the data part
+
+            // Append ownerAndSalt to the end and calculate full code size
+            mstore(add(codeData, codeSize), ownerAndSalt)
+            let fullCodeSize := add(codeSize, 0x20) // Increase size by 32 bytes (size of bytes32)
+
+            // Perform create2 with call value, pointing to the start of the code, its size, and the salt
+            wallet := create2(callvalue(), codeData, fullCodeSize, ownerAndSalt)
+            if iszero(wallet) { revert(0, 0) } // Revert if create2 fails
+        }
     }
 }
