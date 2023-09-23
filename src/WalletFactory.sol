109ea6affab4cb20558c97a7b8d55f7b64babebf
ross <92001561+z0r0z@users.noreply.github.com>
2023-09-23 12:23:20 +0000

    ~~

--- src/WalletFactory.sol
+++ src/WalletFactory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import {Wallet} from './Wallet.sol';
 
 contract WalletFactory {
-    function deploy(bytes32 owner) public payable returns (Wallet wallet) {
+    function deploy(bytes32 owner) public payable returns (Wallet) {
         return new Wallet{value: msg.value, salt: owner}(owner);
     }
 }
