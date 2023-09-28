42f2bbf5b04a07889f4fadc513be98f5632e4897
ross <92001561+z0r0z@users.noreply.github.com>
2023-09-28 13:23:11 +0000

    ~~ expand comments

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -11,16 +11,13 @@ contract Wallet {
 
     // Execute Op...
     function execute(bytes32 to, uint val, bytes calldata data, uint op) public payable {
-        // Load `owner`.
-        bytes32 o = owner;
+        bytes32 o = owner; // Pull `owner` onto stack.
         assembly ("memory-safe") {
             // Only `owner` or `entryPoint` may call this function.
             if and(xor(caller(), o), xor(caller(), entryPoint)) { revert(0, 0) }
-            // Copy `data` to memory.
-            calldatacopy(0, data.offset, data.length)
-            // If any non-zero `op` we use call(). This is done as optimization.
+            calldatacopy(0, data.offset, data.length) // Copy `data` to memory.
             if op {
-                // call() and load `success` state.
+                // If any non-zero `op` we use call().
                 let success := call(gas(), to, val, 0, data.length, 0, 0)
                 // Copy returned data to memory.
                 returndatacopy(0, 0, returndatasize())
@@ -28,8 +25,7 @@ contract Wallet {
                 if iszero(success) { revert(0, returndatasize()) }
                 // Otherwise, return data.
                 return(0, returndatasize())
-            }
-            // If zero `op`, perform delegatecall().
+            } // If zero `op`, perform delegatecall().
             let success := delegatecall(gas(), to, 0, data.length, 0, 0)
             // Copy returned data to memory.
             returndatacopy(0, 0, returndatasize())
@@ -41,27 +37,23 @@ contract Wallet {
     }
 
     // eip-1271...
-    // audit-note: we assume low-level calls and thus don't bother with view.
+    // Audit-Note: We assume low-level calls and thus don't bother with view.
+    // Let's review the security implications though.
     function isValidSignature(bytes32 hash, bytes calldata sig) public payable {
-        // Load `owner`.
-        bytes32 o = owner;
+        bytes32 o = owner; // Pull `owner` onto stack.
         assembly ("memory-safe") {
-            // Load free memory pointer.
-            let m := mload(0x40)
-            // Load `hash` into first slot.
-            mstore(0, hash)
+            mstore(0, hash) // Load `hash` into first slot.
             // Load `v` as 65th byte in `sig` by adding 64 to offset.
             mstore(0x20, byte(0, calldataload(add(sig.offset, 0x40))))
             // Load `r` and `s` in `sig` as 64 bytes from offset.
             calldatacopy(0x40, sig.offset, 0x40)
-            // If the returned data matches `owner` return magic value.
+            // If return data matches `owner` return EIP-1271 magic value.
             if eq(o, mload(staticcall(gas(), 1, 0, 0x80, 0x01, 0x20))) {
-                mstore(0x40, m) // Restore the free memory pointer.
                 mstore(0x20, 0x1626ba7e) // Store magic value.
                 return(0x3C, 0x20) // Return magic value.
             }
-            // Restore the free memory pointer.
-            mstore(0x40, m)
+            // Audit-Note: We don't bother with return as eip-1271 doesn't require
+            // explicit return value for fail case and it will just be null?
         }
     }
 
@@ -85,42 +77,37 @@ contract Wallet {
         bytes32 userOpHash,
         uint // Prefunded.
     ) public payable returns (uint validationData) {
-        // Fetch `sig` item from struct.
+        // Memo `sig` calldata item from struct.
         bytes calldata sig = userOp.signature;
-        // Load `owner`.
-        bytes32 o = owner;
+        bytes32 o = owner; // Pull `owner` onto stack.
         assembly ("memory-safe") {
             // Only `entryPoint` may call this function.
             if xor(caller(), entryPoint) { revert(0, 0) }
-            // Load free memory pointer.
-            let m := mload(0x40)
-            // Load `userOpHash` into first slot.
-            mstore(0, userOpHash)
-            // Load `v` as 65th byte in `sig` by adding 64 to offset.
+            let m := mload(0x40) // Cache free memory pointer.
+            mstore(0, userOpHash) // Memo `userOpHash` into first slot.
+            // Memo `v` as 65th byte in `sig` by adding 64 to offset.
             mstore(0x20, byte(0, calldataload(add(sig.offset, 0x40))))
-            // Load `r` and `s` in `sig` as 64 bytes from offset.
+            // Memo `r` and `s` in `sig` as 64 bytes from offset.
             calldatacopy(0x40, sig.offset, 0x40)
-            // If the returned data matches `owner` return magic value of `0`. Else, `1`.
+            // If return data matches `owner` return magic value of `0`. Else, `1`.
             // This is what the `entryPoint` expects under eip-4337 though unintuitive.
             validationData := xor(o, mload(staticcall(gas(), 1, 0, 0x80, 0x01, 0x20)))
-            // Restore the free memory pointer.
-            mstore(0x40, m)
+            mstore(0x40, m) // Restore the free memory pointer.
         }
     }
 
     // Receivers...
     receive() external payable {}
 
-    // audit-note: This is for `safeTransfer`-type
+    // Audit-Note: This is for `safeTransfer`-type
     // receiver compatibility. Since only `msg.sig`
     // can be returned, risk seems low. Other checks
-    // are punted onto the calling contracts.
+    // are punted onto the calling contracts etc.
     fallback() external payable {
-        assembly {
-            // Shift any unexpected call to `msg.sig`
+        assembly ("memory-safe") {
+            // Shift unexpected call to `msg.sig`.
             mstore(0x20, shr(224, calldataload(0)))
-            // Return `msg.sig`.
-            return(0x3C, 0x20)
+            return(0x3C, 0x20) // Return `msg.sig`.
         }
     }
 }
