0766a0851466386d931de7290bcc43873a0a3778
ross <92001561+z0r0z@users.noreply.github.com>
2023-10-05 10:02:37 +0000

    snip receive into fallback

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -9,7 +9,6 @@ contract Wallet {
         user = _user;
     }
 
-    /// @dev Permissioned call execution logic. Returns data.
     function execute(address to, uint val, bytes calldata data) public payable {
         bytes32 usr = user;
         assembly ("memory-safe") {
@@ -23,7 +22,6 @@ contract Wallet {
         }
     }
 
-    /// @dev Permissioned delegatecall execution logic. Returns data.
     function execute(address to, bytes calldata data) public payable {
         bytes32 usr = user;
         assembly ("memory-safe") {
@@ -37,22 +35,18 @@ contract Wallet {
         }
     }
 
-    /// @dev ERC1271 contract signature validation logic. Returns magic value.
     function isValidSignature(bytes32 hash, bytes calldata signature) public view {
         bytes32 usr = user;
         assembly ("memory-safe") {
-            mstore(0x00, hash) // Place `hash` into first slot.
-            // Assume the `signature` is encoded as `v + r + s`.
+            mstore(0x00, hash)
             calldatacopy(0x20, signature.offset, signature.length)
-            // If the return data matches `user` return ERC1271 magic value.
             if eq(usr, mload(staticcall(gas(), 0x01, 0x00, 0x80, 0x01, 0x20))) {
-                mstore(0x00, 0x1626ba7e) // Store magic value.
-                return(0x1C, 0x04) // Return magic value.
+                mstore(0x00, 0x1626ba7e)
+                return(0x1C, 0x04)
             }
         }
     }
 
-    /// @dev ERC4337 struct.
     struct UserOperation {
         address sender;
         uint nonce;
@@ -67,43 +61,33 @@ contract Wallet {
         bytes signature;
     }
 
-    /// @dev ERC4337 account.
     function validateUserOp(
         UserOperation calldata userOp,
         bytes32 userOpHash,
         uint missingAccountFunds
     ) public payable returns (uint validationData) {
-        // Memo `sig` calldata item from struct.
         bytes calldata sig = userOp.signature;
-        bytes32 usr = user; // Pull `user` onto stack.
+        bytes32 usr = user;
         assembly ("memory-safe") {
-            // Only `entryPoint` may call this function.
             if xor(caller(), entryPoint) { revert(0x00, 0x00) }
-            let m := mload(0x40) // Cache the free memory pointer.
-            mstore(0x00, userOpHash) // Place `userOpHash` into first slot.
-            // Assume the `signature` is encoded as `v + r + s`.
+            let m := mload(0x40)
+            mstore(0x00, userOpHash)
             calldatacopy(0x20, sig.offset, sig.length)
-            // If ecrecover doesn't match `user`, `validationData` is `1`, else `0`.
             if xor(usr, mload(staticcall(gas(), 0x01, 0x00, 0x80, 0x01, 0x20))) {
                 validationData := 1
             }
-            mstore(0x40, m) // Restore the free memory pointer.
-            // Refund the `entryPoint` if any relayer gas is owed.
+            mstore(0x40, m)
             if missingAccountFunds {
                 pop(call(gas(), caller(), missingAccountFunds, 0x00, 0x00, 0x00, 0x00))
             }
         }
     }
 
-    /// @dev Ether (ETH) receiver.
-    receive() external payable {}
-
-    /// @dev Callback handling.
     fallback() external payable {
         assembly ("memory-safe") {
-            // Shift unknown call into its `msg.sig`.
+            if callvalue() { return(0x00, 0x00) }
             mstore(0x20, shr(224, calldataload(0)))
-            return(0x3C, 0x20) // Return `msg.sig`.
+            return(0x3C, 0x20)
         }
     }
 }
