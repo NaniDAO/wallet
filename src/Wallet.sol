731901f94311105039f86ed70a1dc2726adad22d
ross <92001561+z0r0z@users.noreply.github.com>
2023-10-09 11:21:46 +0000

    ~~

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -58,18 +58,21 @@ contract Wallet {
     {
         bytes32 usr = user;
         assembly ("memory-safe") {
-            if xor(caller(), entryPoint) { revert(0x00, 0x00) }
-            let m := mload(0x40)
+            if xor(caller(), entryPoint) { revert(0x00, 0x00) } // Auth.
+            let m := mload(0x40) // Cache free memory pointer.
+            // If nonce is set, extract `to` forwarder in `execute()`.
+            // If `to` is signed, it is trusted to implement user ops.
             if calldataload(0x84) { userOpHash := calldataload(0x208) }
             mstore(0x20, userOpHash) // Store into scratch space for keccak256.
             mstore(0x00, '\x00\x00\x00\x00\x19Ethereum Signed Message:\n32') // 28 bytes.
             mstore(0x00, keccak256(0x04, 0x3c)) // `32 * 2 - (32 - 28) = 60 = 0x3c`.
-            calldatacopy(0x20, sub(calldatasize(), 0x60), 0x60)
+            calldatacopy(0x20, sub(calldatasize(), 0x60), 0x60) // Copy `v, r, s`.
             if xor(usr, mload(staticcall(gas(), 0x01, 0x00, 0x80, 0x01, 0x20))) {
                 validationData := 1
             }
-            mstore(0x40, m)
+            mstore(0x40, m) // Restore free memory pointer.
             if missingAccountFunds {
+                // Refund validation.
                 pop(call(gas(), caller(), missingAccountFunds, 0x00, 0x00, 0x00, 0x00))
             }
         }
