30325bdc82f131cad4c9ac22870e7c29736a2f28
Shivanshi Tyagi <shivanshityagi@icloud.com>
2023-10-17 19:35:57 +0530

    base

--- src/Permissions.sol
+++ src/Permissions.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
+import {ECDSA} from '@solady/src/utils/ECDSA.sol';
 import {EIP712} from '@solady/src/utils/EIP712.sol';
-import '@forge/Test.sol';
+import {ERC4337} from '@solady/test/utils/mocks/MockERC4337.sol';
+import {SignatureCheckerLib} from '@solady/src/utils/SignatureCheckerLib.sol';
+import {LibSort} from '@solady/src/utils/LibSort.sol';
 
-import {Wallet} from '../src/Wallet.sol';
+import '@forge/Test.sol';
 
 enum TYPE
 // uint<M>: unsigned integer type of M bits, 0 < M <= 256, M % 8 == 0
 {
     UINT,
+    UINT8,
     // int<M>: two’s complement signed integer type of M bits, 0 < M <= 256, M % 8 == 0.
     INT,
     // equivalent to uint160, except for the assumed interpretation and language typing. For computing the function selector, address is used.
@@ -23,13 +27,13 @@ enum TYPE
     STRING
 }
 // <type>[]: a variable-length array of elements of the given type.
-
 // (T1,T2,...,Tn): tuple consisting of the types T1, …, Tn, n >= 0
 
 struct Param {
     TYPE _type;
     uint8 offset;
     bytes bounds;
+    uint length;
 }
 
 struct Slip {
@@ -37,10 +41,9 @@ struct Slip {
     uint maxValue;
     bytes4 selector;
     Param[] arguments;
-    string data;
-    uint uses;
-    uint validUntil;
-    uint validAfter;
+    uint192 uses;
+    uint32 validAfter;
+    uint32 validUntil;
 }
 
 struct Call {
@@ -66,69 +69,90 @@ contract Permissions is EIP712 {
         version = '1';
     }
 
-    function checkPermission(Wallet wallet, bytes memory sig, Slip memory slip, Call calldata call)
-        public
-        returns (bool)
-    {
+    function checkPermission(
+        ERC4337 wallet,
+        bytes calldata sig,
+        Slip calldata slip,
+        Call calldata call
+    ) public returns (bool) {
         require(slip.targets.length != 0, 'Permissions: no targets');
         require(slip.uses != 0, 'Permissions: no uses');
-        require(slip.validAfter != 0, 'Permissions: no validAfter');
-        require(slip.validUntil != 0, 'Permissions: no validUntil');
         require(
-            slip.validAfter < slip.validUntil,
-            'Permissions: validAfter must be less than validUntil'
+            slip.validAfter <= slip.validUntil,
+            'Permissions: validAfter must be less than or equal to validUntil'
         );
 
         bytes32 slipHash = getSlipHash(wallet, slip);
         // check if the slip is authorized
-        bytes4 magicValue = wallet.isValidSignature(slipHash, sig);
-        if (magicValue != 0x1626ba7e) return false;
-        // check if slip is used
-        if (slipUses[slipHash] >= slip.uses) return false;
-        // increment slip use count
-        slipUses[slipHash]++;
+        if (!SignatureCheckerLib.isValidERC1271SignatureNowCalldata(address(wallet), slipHash, sig))
+        {
+            return false;
+        }
 
-        // check if the slip is within time bounds
-        if (slip.validAfter > block.timestamp || slip.validUntil < block.timestamp) return false;
+        // check if slip is used up after increment
+        unchecked {
+            if (slip.uses == slipUses[slipHash]++) return false;
+        }
+
+        // check if slip is within valid time bounds or infinite
+        if ((slip.validAfter != 0 && block.timestamp < slip.validAfter) || 
+            (slip.validUntil != 0 && block.timestamp > slip.validUntil)) return false;
+
         // check if call target is authorized
-        for (uint i = 0; i < slip.targets.length; i++) {
-            if (slip.targets[i] == call.to) break;
-            if (i == slip.targets.length - 1) return false;
+        unchecked {
+            for (uint i; i < slip.targets.length; ++i) {
+                if (slip.targets[i] == call.to) break;
+                if (i == slip.targets.length - 1) return false;
+            }
         }
 
         // check if the call is within value bounds
         if (call.value > slip.maxValue) return false;
 
         // check selector
-        if (slip.selector.length != 0) {
-            if (bytes4(call.data[:4]) != slip.selector) return false;
+        if (slip.selector.length != 0 && call.data.length != 0) {
+            if (bytes4(call.data[:4]) != bytes4(slip.selector)) return false;
 
             // check if the call is within data bounds
-            for (uint i = 0; i < slip.arguments.length; i++) {
-                Param memory param = slip.arguments[i];
-                
+            for (uint i; i < slip.arguments.length; i++) {
+                Param calldata param = slip.arguments[i];
+
                 if (param._type == TYPE.UINT) {
-                    bytes memory data = call.data[param.offset:4];
-                    uint max = abi.decode(param.bounds, (uint));
-                    uint value = abi.decode(call.data[param.offset:], (uint));
-                    if (value > max) return false;
-                } 
-                // else if (param._type == TYPE.INT) {
-                //     int max = abi.decode(param.bounds, (int));
-                //     int value = abi.decode(call.data[param.offset:], (int));
-
-                //     if (value > max) return false;
-                // } else if (param._type == TYPE.ADDRESS) {
-                //     address bound = abi.decode(param.bounds, (address));
-                //     address value = abi.decode(call.data[param.offset:], (address));
-
-                //     if (bound != value) return false;
-                // } else if (param._type == TYPE.BOOL) {
-                //     bool bound = abi.decode(param.bounds, (bool));
-                //     bool value = abi.decode(call.data[param.offset:], (bool));
-
-                //     if (bound != value) return false;
-                // } else if (param._type == TYPE.BYTES) {
+                    if (_validateUint(abi.decode(call.data[param.offset:param.offset + 32], (uint)), param.bounds)) break;
+                    return false;
+                } else if (param._type == TYPE.UINT8) {
+                    console.log(i, 'param is uint8');
+                    uint8[] memory allowed = new uint8[](param.bounds.length/32);
+                    for (uint j; j < param.bounds.length / 32; j++) {
+                        allowed[j] = abi.decode(param.bounds[j * 32:j * 32 + 32], (uint8));
+                    }
+                    // (uint8[] memory allowed) = abi.decode(param.bounds[0:32], (uint8[]));
+                    // console2.log(uint256(allowed));
+                    uint8 value = abi.decode(call.data[param.offset:param.offset + 32], (uint8));
+                    for (uint j; j < allowed.length; j++) {
+                        if (allowed[j] == value) break;
+                        if (j == allowed.length - 1) return false;
+                    }
+                } else if (param._type == TYPE.INT) {
+                    console.log(i, 'param is int');
+                    (int min, int max) = abi.decode(param.bounds, (int, int));
+                    int value = abi.decode(call.data[param.offset:param.offset + 32], (int));
+                    if (value > max || value < min) return false;
+                } else if (param._type == TYPE.ADDRESS) {
+                    console.log(i, 'param is address');
+                    address value = abi.decode(call.data[param.offset:param.offset + 32], (address));
+                    console.logAddress(value);
+                    bool valid = _validateAddress(value, param.bounds);
+                    if (valid) break;
+                    return false;
+                } else if (param._type == TYPE.BOOL) {
+                    console.log(i, 'param is bool');
+                    bool bound = abi.decode(param.bounds, (bool));
+                    bytes calldata data = bytes(call.data[param.offset:param.offset + 32]);
+                    bool value = abi.decode(data, (bool));
+                    if (bound != value) return false;
+                }
+                // else if (param._type == TYPE.BYTES) {
                 //     bytes memory bound = abi.decode(param.bounds, (bytes));
                 //     bytes memory value = abi.decode(call.data[param.offset:], (bytes));
 
@@ -145,7 +169,34 @@ contract Permissions is EIP712 {
         return true;
     }
 
-    function getSlipHash(Wallet wallet, Slip memory slip) public view returns (bytes32) {
+    function getSlipHash(ERC4337 wallet, Slip calldata slip) public view returns (bytes32) {
         return _hashTypedData(keccak256(abi.encode(wallet, slip)));
+    }
+
+    function _validateUint(uint value, bytes memory bounds)
+        internal
+        pure
+        returns (bool found)
+    {
+        (uint min, uint max) = abi.decode(bounds, (uint, uint));
+        return value >= min && value <= max;
+    }
+
+    function _validateEnum(uint256 value, bytes memory bounds) internal pure returns (bool found) {
+        (uint256[] memory bound) = abi.decode(bounds, (uint256[]));
+        LibSort.sort(bound);
+        (found,) = LibSort.searchSorted(bound, value);
+        return found;
+    }
+
+    function _validateAddress(address value, bytes memory bounds)
+        internal
+        view
+        returns (bool found)
+    {
+        (address[] memory bound) = abi.decode(bounds, (address[]));
+        LibSort.sort(bound);
+        (found,) = LibSort.searchSorted(bound, value);
+        return found;
     }
 }
--- test/Permissions.t.sol
+++ test/Permissions.t.sol
@@ -2,58 +2,381 @@
 pragma solidity ^0.8.19;
 
 import '@forge/Test.sol';
-import {Permissions, Slip, Call, Param} from '../src/Permissions.sol';
+import '@solady/test/utils/TestPlus.sol';
+import {LibClone} from '@solady/src/utils/LibClone.sol';
+import {SignatureCheckerLib} from '@solady/src/utils/SignatureCheckerLib.sol';
+
+import {ERC4337, MockERC4337} from '@solady/test/utils/mocks/MockERC4337.sol';
+import {MockERC20} from '@solady/test/utils/mocks/MockERC20.sol';
+import {LibSort} from '@solady/src/utils/LibSort.sol';
+
+import {Permissions, Slip, Call, Param, TYPE} from '../src/Permissions.sol';
 import {Wallet} from '../src/Wallet.sol';
 
-contract PermissionsTest is Test {
+contract PermissionsTester {
+    enum State {
+        PENDING,
+        APPROVED,
+        REJECTED,
+        CANCELLED,
+        EXECUTED,
+        EXPIRED,
+        FAILED
+    }
+
+    constructor() {}
+
+    function dataUint(uint data) public pure returns (uint) {
+        return data;
+    }
+
+    function dataInt(int data) public pure returns (int) {
+        return data;
+    }
+
+    function dataAddress(address data) public pure returns (address) {
+        return data;
+    }
+
+    function dataValue() public payable returns (uint) {
+        return msg.value;
+    }
+
+    function dataBool(bool data) public pure returns (bool) {
+        return data;
+    }
+}
+
+contract PermissionsTest is Test, TestPlus {
     Permissions permissions;
-    Wallet wallet;
+    MockERC4337 wallet;
 
     address alice;
-    bytes32 aliceHash;
     uint aliceKey;
+
+    address bob;
+    uint bobKey;
 
     function setUp() public payable {
         (alice, aliceKey) = makeAddrAndKey('alice');
 
-        aliceHash = bytes32(uint(uint160(alice)));
-
         permissions = new Permissions();
-        wallet = new Wallet(aliceHash);
+
+        address accountImplementation = address(new MockERC4337());
+        wallet = MockERC4337(payable(LibClone.deployERC1967(accountImplementation)));
+        wallet.initialize(alice);
     }
 
-    function testCheckPermission() public {
-        address[] memory targets = new address[](1);
-        targets[0] = address(this);
-        Slip memory slip = createSlip(targets, 1, block.timestamp, block.timestamp + 1000);
-        Call memory call = Call({to: address(this), value: 0, data: ''});
+    function testValuePermission(uint value, uint maxValue) public {
+        vm.assume(value <= type(uint128).max);
+        vm.assume(maxValue <= type(uint128).max);
+        address[] memory targets = getTargets(0, alice);
+        console.log(targets[0]);
+        Slip memory slip = createSlip(
+            targets,
+            maxValue,
+            bytes4(0),
+            new Param[](0),
+            1,
+            uint32(block.timestamp),
+            uint32(block.timestamp + 1000)
+        );
+        Call memory call = Call({to: alice, value: value, data: ''});
+        bytes32 slipHash = permissions.getSlipHash(wallet, slip);
+
+        bytes memory sig = sign(aliceKey, SignatureCheckerLib.toEthSignedMessageHash(slipHash));
+        bool assertion = value <= slip.maxValue;
+        assertEq(permissions.checkPermission(wallet, sig, slip, call), assertion);
+    }
+
+    function testTimePermissions(uint32 validAfter, uint32 validUntil) public {
+        vm.assume(validAfter <= type(uint32).max);
+        vm.assume(validUntil <= type(uint32).max);
+        vm.assume(validAfter <= validUntil);
+        require(validAfter <= type(uint32).max);
+        require(validUntil <= type(uint32).max);
+        require(validAfter <= validUntil);
+
+        address[] memory targets = getTargets(0, alice);
+        Slip memory slip = createSlip(
+            targets,
+            0,
+            bytes4(0),
+            new Param[](0),
+            1,
+            uint32(validAfter),
+            uint32(validUntil)
+        );
+        Call memory call = Call({to: alice, value: 0, data: ''});
+        bytes32 slipHash = permissions.getSlipHash(wallet, slip);
+
+        bytes memory sig = sign(aliceKey, SignatureCheckerLib.toEthSignedMessageHash(slipHash));
+        bool assertion = (validAfter == 0 || block.timestamp >= slip.validAfter) && 
+                          (validUntil == 0 || block.timestamp <= slip.validUntil);
+        assertEq(permissions.checkPermission(wallet, sig, slip, call), assertion);
+    }
+
+    function testUintPermission(uint val, uint min, uint max) public {
+        vm.assume(min < max);
+        vm.assume(val <= type(uint).max);
+        vm.assume(min <= type(uint).max);
+        vm.assume(max <= type(uint).max);
+        require(min <= max);
+
+        bool assertion = !(val < min || val > max);
+
+        PermissionsTester tester = new PermissionsTester();
+
+        address[] memory targets = getTargets(0, alice);
+        Param[] memory arguments = new Param[](1);
+        arguments[0] = Param({_type: TYPE.UINT, offset: 4, bounds: abi.encode(min, max), length: 0});
+
+        Slip memory slip = createSlip(
+            targets,
+            0,
+            tester.dataUint.selector,
+            arguments,
+            5,
+            uint32(block.timestamp),
+            uint32(block.timestamp + 100000)
+        );
+
+        Call memory call = Call({to: alice, value: 0, data: abi.encodeCall(tester.dataUint, (val))});
 
         bytes32 slipHash = permissions.getSlipHash(wallet, slip);
-        bytes memory sig = sign(aliceKey, slipHash);
+        bytes memory sig = sign(aliceKey, SignatureCheckerLib.toEthSignedMessageHash(slipHash));
 
-        permissions.checkPermission(wallet, sig, slip, call);
+        assertEq(permissions.checkPermission(wallet, sig, slip, call), assertion);
     }
 
-    function createSlip(address[] memory targets, uint uses, uint validAfter, uint validUntil)
-        public
-        pure
-        returns (Slip memory slip)
-    {
+    // function testCheckTransferPermission() public {
+    //     MockERC20 token = new MockERC20("Token", "TKN", 18);
+
+    //     address[] memory targets = new address[](1);
+    //     targets[0] = address(token);
+
+    //     Param[] memory arguments = new Param[](2);
+    //     arguments[0] = Param({
+    //         _type: TYPE.ADDRESS,
+    //         offset: 4,
+    //         bounds: abi.encodePacked(address(this), alice),
+    //         length: 0
+    //     });
+    //     arguments[1] = Param({
+    //         _type: TYPE.UINT,
+    //         offset: 36,
+    //         bounds: abi.encodePacked(uint(2 ether)),
+    //         length: 0
+    //     });
+
+    //     Slip memory slip = createSlip(
+    //         targets,
+    //         0,
+    //         token.transfer.selector,
+    //         arguments,
+    //         5,
+    //         uint32(block.timestamp),
+    //         uint32(block.timestamp + 100000)
+    //     );
+    //     Call memory call = Call({
+    //         to: address(token),
+    //         value: 0,
+    //         data: abi.encodeWithSelector(token.transfer.selector, address(this), 1.5 ether)
+    //     });
+
+    //     bytes32 slipHash = permissions.getSlipHash(wallet, slip);
+    //     bytes memory sig = sign(aliceKey, SignatureCheckerLib.toEthSignedMessageHash(slipHash));
+
+    //     assertTrue(permissions.checkPermission(wallet, sig, slip, call));
+    // }
+
+    // function testFailCheckTransferPermission() public {
+    //     MockERC20 token = new MockERC20("Token", "TKN", 18);
+
+    //     address[] memory targets = new address[](1);
+    //     targets[0] = address(token);
+
+    //     Param[] memory arguments = new Param[](2);
+    //     arguments[0] = Param({
+    //         _type: TYPE.ADDRESS,
+    //         offset: 4,
+    //         bounds: abi.encodePacked(address(this), alice),
+    //         length: 0
+    //     });
+    //     arguments[1] = Param({
+    //         _type: TYPE.UINT,
+    //         offset: 36,
+    //         bounds: abi.encodePacked(uint(2 ether)),
+    //         length: 0
+    //     });
+
+    //     Slip memory slip = createSlip(
+    //         targets,
+    //         0,
+    //         token.transfer.selector,
+    //         arguments,
+    //         5,
+    //         uint32(block.timestamp),
+    //         uint32(block.timestamp + 100000)
+    //     );
+    //     Call memory call = Call({
+    //         to: bob,
+    //         value: 0,
+    //         data: abi.encodeWithSelector(token.transfer.selector, address(this), 1.5 ether)
+    //     });
+
+    //     bytes32 slipHash = permissions.getSlipHash(wallet, slip);
+    //     bytes memory sig = sign(aliceKey, SignatureCheckerLib.toEthSignedMessageHash(slipHash));
+
+    //     assertTrue(permissions.checkPermission(wallet, sig, slip, call));
+    // }
+
+    function testBoolPermissions(bool value, bool bound) public {
+        PermissionsTester tester = new PermissionsTester();
+
+        address[] memory targets = getTargets(0, alice);
+
+        Param[] memory arguments = new Param[](1);
+        arguments[0] = Param({_type: TYPE.BOOL, offset: 4, bounds: abi.encode(bound), length: 0});
+
+        Slip memory slip = createSlip(
+            targets,
+            0,
+            tester.dataBool.selector,
+            arguments,
+            5,
+            uint32(block.timestamp),
+            uint32(block.timestamp + 100000)
+        );
+        Call memory call =
+            Call({to: alice, value: 0, data: abi.encodeCall(tester.dataBool, (value))});
+
+        bytes32 slipHash = permissions.getSlipHash(wallet, slip);
+        bytes memory sig = sign(aliceKey, SignatureCheckerLib.toEthSignedMessageHash(slipHash));
+
+        assertEq(permissions.checkPermission(wallet, sig, slip, call), value == bound);
+    }
+
+    function testAddressPermission(address value, address[] memory bounds) public {
+        vm.assume(bounds.length != 0);
+        vm.assume(bounds.length <= type(uint8).max);
+        require(bounds.length != 0);
+        require(bounds.length <= type(uint8).max);
+
+        LibSort.sort(bounds);
+        (bool assertion, uint index) = LibSort.searchSorted(bounds, value);
+        console.log('found', assertion);
+        console.log('index', index);
+        console.log('found value', bounds[index]);
+
+        PermissionsTester tester = new PermissionsTester();
+        address[] memory targets = getTargets(0, alice);
+        Param[] memory arguments = new Param[](1);
+        arguments[0] =
+            Param({_type: TYPE.ADDRESS, offset: 4, bounds: abi.encode(bounds), length: 0});
+
+        Slip memory slip = createSlip(
+            targets,
+            0,
+            tester.dataAddress.selector,
+            arguments,
+            5,
+            uint32(block.timestamp),
+            uint32(block.timestamp + 100000)
+        );
+
+        Call memory call =
+            Call({to: alice, value: 0, data: abi.encodeCall(tester.dataAddress, (value))});
+
+        bytes32 slipHash = permissions.getSlipHash(wallet, slip);
+
+        bytes memory sig = sign(aliceKey, SignatureCheckerLib.toEthSignedMessageHash(slipHash));
+        console.log('length', bounds.length);
+        assertEq(permissions.checkPermission(wallet, sig, slip, call), assertion);
+    }
+
+    function testIntPermission(int a, int min, int max) public {
+        // prevent overflows;
+        a = bound(a, type(int).min, type(int).max);
+        min = bound(min, type(int).min, type(int).max);
+        max = bound(max, type(int).min, type(int).max);
+
+        vm.assume(min < max);
+        vm.assume(a <= type(int).max && a >= type(int).min);
+        vm.assume(min <= type(int).max && min >= type(int).min);
+        vm.assume(max <= type(int).max && max >= type(int).min);
+        require(min <= max);
+
+        bool assertion = !(a > max || a < min);
+
+        PermissionsTester tester = new PermissionsTester();
+
+        address[] memory targets = new address[](1);
+        targets[0] = address(tester);
+
+        Param[] memory arguments = new Param[](1);
+        arguments[0] = Param({_type: TYPE.INT, offset: 4, bounds: abi.encode(min, max), length: 0});
+
+        Slip memory slip = createSlip(
+            targets,
+            0,
+            tester.dataInt.selector,
+            arguments,
+            5,
+            uint32(block.timestamp),
+            uint32(block.timestamp + 100000)
+        );
+
+        Call memory call =
+            Call({to: address(tester), value: 0, data: abi.encodeCall(tester.dataInt, (a))});
+
+        bytes32 slipHash = permissions.getSlipHash(wallet, slip);
+        bytes memory sig = sign(aliceKey, SignatureCheckerLib.toEthSignedMessageHash(slipHash));
+
+        assert(permissions.checkPermission(wallet, sig, slip, call) == assertion);
+    }
+
+    function createSlip(
+        address[] memory targets,
+        uint maxValue,
+        bytes4 selector,
+        Param[] memory arguments,
+        uint192 uses,
+        uint32 validAfter,
+        uint32 validUntil
+    ) public pure returns (Slip memory slip) {
         return Slip({
             targets: targets,
-            maxValue: 0,
-            selector: bytes4(keccak256('testCheckPermission()')),
-            arguments: new Param[](0),
-            data: '',
+            maxValue: maxValue,
+            selector: selector,
+            arguments: arguments,
             uses: uses,
             validUntil: validUntil,
             validAfter: validAfter
         });
     }
 
+    function getTargets(uint8 length, address include)
+        internal
+        returns (address[] memory targets)
+    {
+        bool toInclude = include != address(0);
+        targets = new address[](toInclude ? length + 1 : length);
+
+        if (length != 0) {
+            for (uint8 i = 0; i < length - 1; i++) {
+                targets[i] = _randomAddress();
+            }
+        }
+
+        if (toInclude) {
+            targets[length] = include;
+        }
+    }
+
     function sign(uint pK, bytes32 hash) internal pure returns (bytes memory) {
         // Helper.
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(pK, hash);
-        return abi.encode(v, r, s);
+        return abi.encodePacked(r, s, v);
     }
 }
